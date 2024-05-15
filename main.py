3b4089e8b7ce7a2269a438dcabfdaf504d24e121
JacobWood12 <525JacobW@gmail.com>
2024-05-15 14:45:00 +1200

    Started on buttons and tasks frames.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 """ Tkinter is for the GUI. """
 from tkinter import Tk, Label, Frame
 
-W_WIDTH = 300
+W_WIDTH = 500
 W_HEIGHT = 300
 
 window = Tk()
 window.geometry = str(W_WIDTH) + "x" + str(W_HEIGHT)
 window.title("Application")
 
-main_frame = Frame(background="red", width=W_WIDTH, height=W_HEIGHT)
-main_frame.pack()
+# This is where the list of tasks will go.
+tasks_frame = Frame(background="#FFFFFF", width=W_WIDTH, height=W_HEIGHT)
+tasks_frame.pack()
+
+# This is where the buttons at the bottom will go.
+buttons_frame = Frame(background="#D9D9D9")
+buttons_frame.pack()
 
 hello_label = Label(text="Hello World!")
 hello_label.place(x=300, y=300)
