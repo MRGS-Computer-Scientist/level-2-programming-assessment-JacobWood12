1b1906b6614b40cd528629b7e1a8dca512f78747
JacobWood12 <525JacobW@gmail.com>
2024-05-15 14:33:03 +1200

    Replaced window geometry with variables.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """ Tkinter is for the GUI. """
 from tkinter import Tk, Label
 
+W_WIDTH = 300
+W_HEIGHT = 300
+
 window = Tk()
-window.geometry = "300x300"
+window.geometry = W_WIDTH + W_HEIGHT
 window.title("Application")
 
 hello_label = Label(text="Hello World!")
