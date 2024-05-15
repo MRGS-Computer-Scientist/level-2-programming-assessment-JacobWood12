901a786429d05f741698ad64637b73518f1fa28d
JacobWood12 <525JacobW@gmail.com>
2024-05-15 14:24:31 +1200

    Started with grid.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 """ Tkinter is for the GUI. """
-from tkinter import Tk
+from tkinter import Tk, Label
 
 window = Tk()
 window.geometry = "300x300"
 window.title("Application")
+
+# Defines layout manager as grid.
+window.grid()
+
+hello_label = Label(text="Hello World!")
+hello_label.grid(column=0,row=0)
 
 window.mainloop()
