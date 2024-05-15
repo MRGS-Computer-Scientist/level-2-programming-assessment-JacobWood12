429387d079abe11777ee8b48c68c69456b8ca19a
JacobWood12 <525JacobW@gmail.com>
2024-05-15 14:35:09 +1200

    Added a frame.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 """ Tkinter is for the GUI. """
-from tkinter import Tk, Label
+from tkinter import Tk, Label, Frame
 
 W_WIDTH = 300
 W_HEIGHT = 300
 
 window = Tk()
-window.geometry = W_WIDTH + W_HEIGHT
+window.geometry = str(W_WIDTH) + "x" + str(W_HEIGHT)
 window.title("Application")
+
+main_frame = Frame(background="red", width=W_WIDTH, height=W_HEIGHT)
+main_frame.pack()
 
 hello_label = Label(text="Hello World!")
 hello_label.place(x=300, y=300)
