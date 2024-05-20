3876fb7bd0587eaab46d9d6a47b47e97486c9c2e
Jacob Wood <21108@students.mrgs.school.nz>
2024-05-21 09:12:04 +1200

    Merge Tkintertesting into main (#1)
    * Started with grid.
    
    * Added goodbye label
    
    * Started with place.
    
    Added a hello label.
    
    * Replaced window geometry with variables.
    
    * Added a frame.
    
    * Started on buttons and tasks frames.
    
    * Added comments

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 """ Tkinter is for the GUI. """
-from tkinter import Tk
+from tkinter import Tk, Label, Frame
 
+# Defines height and width of the window
+w_width = 500
+w_height = 300
+
+# Sets up window.
 window = Tk()
-window.geometry = "300x300"
+window.geometry = str(w_width) + "x" + str(w_height)
 window.title("Application")
+
+# This is where the list of tasks will go.
+tasks_frame = Frame(background="#FFFFFF", width=w_width, height=w_height)
+tasks_frame.pack()
+
+# This is where the buttons at the bottom will go.
+buttons_frame = Frame(background="#D9D9D9")
+buttons_frame.pack()
+
+# Sets up label.
+hello_label = Label(text="Hello World!")
+hello_label.place(x=300, y=300)
 
 window.mainloop()
