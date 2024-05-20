17d9d5ecb39966cd6e7c9ef7f7d5a7351324615c
JacobWood12 <525JacobW@gmail.com>
2024-05-21 09:17:02 +1200

    Removed hello world and started basic layout.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """ Tkinter is for the GUI. """
-from tkinter import Tk, Label, Frame
+from tkinter import Tk, Frame
 
 # Defines height and width of the window
 w_width = 500
-w_height = 300
+w_height = 750
 
 # Sets up window.
 window = Tk()
@@ -11,15 +11,11 @@ window.geometry = str(w_width) + "x" + str(w_height)
 window.title("Application")
 
 # This is where the list of tasks will go.
-tasks_frame = Frame(background="#FFFFFF", width=w_width, height=w_height)
+tasks_frame = Frame(background="#FFFFFF", width=w_width, height=650)
 tasks_frame.pack()
 
 # This is where the buttons at the bottom will go.
-buttons_frame = Frame(background="#D9D9D9")
+buttons_frame = Frame(background="#D9D9D9", width=w_width, height=150)
 buttons_frame.pack()
-
-# Sets up label.
-hello_label = Label(text="Hello World!")
-hello_label.place(x=300, y=300)
 
 window.mainloop()
