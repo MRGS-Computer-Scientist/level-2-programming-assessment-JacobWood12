769488fc268baf8d695eb1e52b4dc354a5ab3cd9
JacobWood12 <525JacobW@gmail.com>
2024-05-23 12:54:28 +1200

    Added first task button

--- main.py
+++ main.py
@@ -26,4 +26,8 @@ buttons_frame.pack()
 addtask_button = Button(buttons_frame, text="+ | Add Task", command=addtask, bg="#D9D9D9")
 addtask_button.place(relx=0.5, rely=0.5, anchor="center")
 
+# Task button.
+task_button = Button(tasks_frame, text="Task 1")
+task_button.pack()
+
 window.mainloop()
